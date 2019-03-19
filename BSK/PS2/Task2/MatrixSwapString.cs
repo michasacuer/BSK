3b90d6ecf7cfc73a6cf06cdc8f60a8cc9deaa484
Michał Szewczak <michasacuer3@gmail.com>
2019-03-19 13:17:54 +0100

    2B WORKS

--- BSK/PS2/Task2/MatrixSwapString.cs
+++ BSK/PS2/Task2/MatrixSwapString.cs
@@ -24,12 +24,15 @@ namespace BSK.PS2
 
         public string Encrypt(string input)
         {
+            input = input.ToUpper();
             string result = string.Empty;
             int maxResultLength = (int)Math.Ceiling((double)input.Length / key.Length) * key.Length;
+            int columnHeight = (int)Math.Ceiling((double)input.Length / key.Length);
+            char[,] chars = new char[key.Length, columnHeight];
 
-            for (int i = 0; i < key.Length; i++)
+            for (int i = 0; i < input.Length; i++)
             {
-                for(int j = 0; j < key.Length; j++)
+                for(int j = 0; j < input.Length; j++)
                 {
                     int stride = j * key.Length;
                     if (mappedKey[i] + stride >= input.Length)
@@ -40,6 +43,7 @@ namespace BSK.PS2
                     }
                     else
                         result += input[mappedKey[i] + stride];
+
                 }
                 if (result.Length >= input.Length)
                     break;
@@ -50,23 +54,28 @@ namespace BSK.PS2
 
         public string Decrypt(string input)
         {
+            input = input.ToUpper();
             string result = string.Empty;
             int columnHeight = (int)Math.Ceiling((double)input.Length / key.Length);
-
             char[,] chars = new char[key.Length, columnHeight];
+            char[,] decryptChars = new char[key.Length, columnHeight];
+            var reversedDict = mappedKey.ToDictionary(a => a.Value, a => a.Key);
 
             int charCounter = 0;
-            for(int i = 0; i < chars.GetLength(0); i++)
-                for(int j = 0; j < chars.GetLength(1); j++)
+            for (int i = 0; i < chars.GetLength(0); i++)
+                for (int j = 0; j < chars.GetLength(1); j++)
                     chars[i, j] = input[charCounter++];
+            
+            for (int i = 0; i < chars.GetLength(0); i++)
+                for (int j = 0; j < chars.GetLength(1); j++)
+                    decryptChars[i, j] = chars[reversedDict[i], j];
 
-            for(int i = 0; i < chars.GetLength(0); i++)
-                for(int j = 0; j < chars.GetLength(1); j++)
-                {
-                    result += chars[i, j];
-                }
+            for (int i = 0; i < chars.GetLength(1); i++)
+                for (int j = 0; j < chars.GetLength(0); j++)
+                    result += decryptChars[j, i];
 
             return result;
+            
         }
 
         private string key { get; set; }
